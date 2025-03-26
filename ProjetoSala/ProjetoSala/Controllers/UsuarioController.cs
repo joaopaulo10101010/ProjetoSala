bee38204df1c6a46111acb85ce3867c89110b11a
Joao Paulo <joao_paulo_oandrade@hotmail.com>
2025-03-26 20:37:26 -0300

    adicionando os construtores

---
+++ ProjetoSala/ProjetoSala/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoSala.Controllers
+{
+    public class UsuarioController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

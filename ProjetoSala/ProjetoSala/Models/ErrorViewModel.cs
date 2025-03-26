70f64eec3386e5cf9ce096e5e18b123d995bb4e0
Joao Paulo <joao_paulo_oandrade@hotmail.com>
2025-03-26 19:45:58 -0300

    Colocando as Bibliotecas do MySQL

---
+++ ProjetoSala/ProjetoSala/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoSala.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
